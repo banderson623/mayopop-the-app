6d3c0c7b9ac6aa628c952207bd7fa1712200665f
Brian Anderson <banderson623@gmail.com>
2015-08-12 22:39:44 -0500

    Initial Commit

---
+++ mayopop-the-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mayopop-the-app
+//
+//  Created by Brian Anderson on 8/12/15.
+//  Copyright (c) 2015 Brian Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
